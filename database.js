9b797a26913df984ce3eb53f16b4475d7a47c05d
Atticus C <atticus@softwarefuze.com>
2021-07-28 07:32:05 -0700

    Setup PostgreSQL database and .env

---
+++ database.js
@@ -0,0 +1,36 @@
+const pg = require("pg");
+const Pool = pg.Pool;
+require("dotenv").config();
+
+// extract env variables from .env
+const { POSTGRES_USER, POSTGRES_PASS, DATABASE } = process.env;
+
+// create database pool that can be queried from
+const pool = new Pool({
+    host: "localhost",
+    port: 5432,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASS,
+    database: DATABASE,
+});
+
+// validate all inputs and make sure they're not vulnerable to any sort of SQL injection
+const validateInputs = ([...inputs] = []) => {
+    const a = new RegExp("w*((%27)|('))((%6F)|o|(%4F))((%72)|r|(%52))", "i");
+    const b = new RegExp("(%27)|(')|(--)|(%23)|(#)", "i");
+    // eslint-disable-next-line
+    const c = new RegExp("(()|(=))[^\n]*((%27)|(')|(--)|(%3B)|(;))", "i");
+    const d = new RegExp("((%27)|('))union", "i");
+    const e = new RegExp("(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})", "gi");
+
+    // loop through inputs and output an array of true values if SQL injection detected
+    const inputsFiltered = inputs.filter((input) =>
+        (a.test(input) || b.test(input) || c.test(input) || d.test(input) || e.test(input)) && true
+    );
+
+    return !(inputsFiltered.length > 0);
+};
+
+module.exports = {
+    pool, validateInputs
+};
---
+++ database.sql
@@ -0,0 +1,25 @@
+-- ShareFrame PostgreSQL Database
+
+-- Establish ShareFrame main Database
+CREATE DATABASE shareframe_db;
+
+-- Create shareframe_db necessary tables
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    user_email VARCHAR(100) NOT NULL,
+    user_pass VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE unv_users (
+    uuser_id SERIAL PRIMARY KEY,
+    uuser_name VARCHAR(50) NOT NULL,
+    uuser_email VARCHAR(100) NOT NULL,
+    uuser_pass VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE rt_banishlist (
+    rt_id SERIAL PRIMARY KEY,
+    rt VARCHAR(512) NOT NULL
+);
