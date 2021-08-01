e84c8e501d291481709abff6a89bb117fa7ea1ec
Atticus C <atticus@softwarefuze.com>
2021-08-01 06:36:57 -0700

    Added refresh token usage

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -36,4 +36,39 @@ router.post('/validateToken', async (req, res) => {
     }
 });
 
+router.post('/refresh', async (req, res) => {
+    const { token } = req.body;
+
+    const exists = await db.query('SELECT * FROM rt_banishlist WHERE rt = $1', [ token ]);
+    if (exists.rows.length > 0) {
+        res.status(403).json({ message: "Invalid Refresh Token" });
+        return;
+    }
+
+    try {
+        const valid = await jwt.verify(token, process.env.RT_SECRET);
+        if (!valid) throw 'Invalid Refresh Token';
+
+        if (valid.hasOwnProperty('id') && valid.hasOwnProperty('password')) {
+            if (validate([ valid['id'], valid['password'] ])) {
+                const rows = await db.query('SELECT * FROM users WHERE user_id = $1', [ valid['id'] ]);
+
+                if (rows.rows.length > 0) {
+                    const validPass = await bcrypt.compare(valid['password'], rows.rows[0].user_pass);
+
+                    if (validPass) {
+                        const at = await jwt.sign(rows.rows[0], process.env.AT_SECRET, { expiresIn: 60 * 15 });
+                        const rt = await jwt.sign({ id: valid['id'], password: valid['password'] }, process.env.RT_SECRET, { expiresIn: 60 * 60 * 24 * 30 });
+                        await db.query('INSERT INTO rt_banishlist (rt) VALUES ($1)', [ token ]);
+
+                        res.status(200).json({ at, rt, account: rows.rows[0] });
+                    } else res.status(403).json({ message: "Invalid Password" });
+                } else throw 'Invalid Refresh Token';
+            } else throw 'Invalid Fields';
+        } else throw 'Invalid Refresh Token';
+    } catch (err) {
+        res.status(400).json({ message: "Invalid request", err });
+    }
+});
+
 module.exports = router;
